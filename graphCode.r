3813b3dd41dbad96244b841556dbbd2446e3c677
zsdawson <65880961+zsdawson@users.noreply.github.com>
2022-02-10 22:29:03 -0800

    Update graphCode.r

--- graphCode.r
+++ graphCode.r
@@ -1,7 +1,8 @@
 library(tidyverse)
 
+x <- read_csv("/Users/#/Desktop/y21-walnut-traps-ssjv.csv")
 ggplot(data = x,
        mapping = aes(x = site,
                      y = now_total,
-                     color = trap_type)) +
+                     color = trap_type))+
   geom_point(size = 1)
